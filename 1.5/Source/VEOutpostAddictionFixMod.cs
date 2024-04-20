3da6a1b2704643931465e1538689876c3e985de1
Taranchuk <nfhfyxer@gmail.com>
2024-04-20 13:04:00 +0300

    update

---
+++ 1.5/Source/VEOutpostAddictionFixMod.cs
@@ -0,0 +1,71 @@
+﻿using HarmonyLib;
+using Outposts;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace VEOutpostAddictionFix
+{
+    public class VEOutpostAddictionFixMod : Mod
+    {
+        public VEOutpostAddictionFixMod(ModContentPack pack) : base(pack)
+        {
+			new Harmony("VEOutpostAddictionFixMod").PatchAll();
+        }
+    }
+
+    [HarmonyPatch]
+    public static class Hediff_Tick_Patch
+    {
+        [HarmonyTargetMethods]
+        public static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.Method(typeof(Hediff), nameof(Hediff.Tick));
+            yield return AccessTools.Method(typeof(Hediff), nameof(Hediff.PostTick));
+        }
+        public static bool Prefix(Hediff __instance)
+        {
+            if (Outpost_OutpostHealthTick_Patch.tickedPawn == __instance.pawn)
+            {
+                if (__instance is Hediff_ChemicalDependency || __instance is Hediff_Addiction)
+                {
+                    __instance.Severity = 0;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Outpost), nameof(Outpost.OutpostHealthTick))]
+    public static class Outpost_OutpostHealthTick_Patch
+    {
+        public static Pawn tickedPawn;
+        public static void Prefix(Pawn pawn)
+        {
+            tickedPawn = pawn;
+        }
+
+        public static void Postfix()
+        {
+            tickedPawn = null;
+        }
+    }
+
+    [HarmonyPatch(typeof(Outpost), nameof(Outpost.SatisfyNeeds), new Type[] { typeof(Pawn) })]
+    public static class Outpost_SatisfyNeeds_Patch
+    {
+        public static void Postfix(Pawn pawn)
+        {
+            if (pawn.needs?.needs != null)
+            {
+                foreach (var need in pawn.needs.needs)
+                {
+                    need.CurLevel = need.MaxLevel;
+                }
+            }
+        }
+    }
+}
